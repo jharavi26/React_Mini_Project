3b678f849b8be4d89436e90110ca4706a4b23fc3
Ravikumar Jha <jharavi153@gmail.com>
2024-12-03 16:17:29 +0530

    Routing in React

---
+++ Recat_Router/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src="" alt=""/>
+      <ul>
+        <li>Home </li>
+        <li>Products</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <button>Get Started</button>
+      
+    </div>
+  )
+}
+
+export default Navbar
