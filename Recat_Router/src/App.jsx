63e48dedddb1707f38a9c3f511de166be3c3132a
Ravikumar Jha <jharavi153@gmail.com>
2024-12-03 18:12:39 +0530

    Final Commit

--- Recat_Router/src/App.jsx
+++ Recat_Router/src/App.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Routes, Route } from 'react-router-dom'
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import RootLayout from './layout/RootLayout'
 
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout />}>
+        <Route index element={<Home />}/>
+        <Route path='products' element={<Products />}/>
+        <Route path='about' element={<About />}/>
+        <Route path='contact' element={<Contact/>}/>
+
+      </Route>)
+  )
   return (
-    <div>
-      <Navbar />
-      <div className='container'>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/products' element={<Products />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='/contact' element={<Contact/>}/>
-        
-      </Routes>
-      </div>
-      
-      
-    </div>
+    <RouterProvider router={router}/>
+   
   )
 }
 
--- Recat_Router/src/components/Navbar.jsx
+++ Recat_Router/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <img src="https://www.shutterstock.com/shutterstock/photos/2278726727/display_1500/stock-vector-minimalistic-circular-logo-sample-vector-2278726727.jpg" style={{ width: '100px', height: '80px' }} alt="Icon Image"/>
       <ul>
-        <li>Home </li>
-        <li>Products</li>
-        <li>About</li>
-        <li>Contact</li>
+        <Link to='/'><li>Home </li></Link>
+        <Link to='/products'><li>Products</li></Link>
+        <Link to='/about'><li>About</li></Link>
+        <Link to='/contact'><li>Contact</li></Link>
       </ul>
       <button>Get Started</button>
       
