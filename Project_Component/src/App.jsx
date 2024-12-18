2ab99aee0d518c7534505ecede1a1c1e3f452766
Ravikumar Jha <jharavi153@gmail.com>
2024-12-18 13:28:12 +0530

    Create Hello World

---
+++ Project_Component/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Hello from './component/Hello'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>Project-Component Building</h1>
+    <Hello />
+    
+      
+    </>
+  )
+}
+
+export default App
---
+++ Project_Component/src/component/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div>
+      <h1>This is Hello World Page</h1>
+      
+    </div>
+  )
+}
+
+export default Hello
