da044f77a8cfd1773be63a46f06749e8ebc4fb2b
Ravikumar Jha <jharavi153@gmail.com>
2024-12-20 17:12:08 +0530

    Add Functionality

--- Project_Component/src/component/Hello.jsx
+++ Project_Component/src/component/Hello.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Hello() {
+  const [input, setInput] = useState("");
   return (
     <div>
       <h1>This is Hello World Page</h1>
+      <form>
+     
+        <input type="text" value={input} onChange={(e)=>setInput(e.target.value)}/>
+        <p>User Input :- {input} </p>
+
+        </form>
+     
       
     </div>
   )
--- Project_Component/src/component/Two.jsx
+++ Project_Component/src/component/Two.jsx
@@ -2,12 +2,20 @@ import React, { useState } from 'react'
 
 export default function Two() {
   const [count, setCount] = useState(0);
+  const fruits = ['Apple', 'Banana', 'Mango', 'Orange', 'Pineapple']
   return (
     <div>
-      <button style={{margin:"10px"}} onClick={()=>setCount(count+1)}>Add Me </button>
+      <button style={{margin:"10px"}} onClick={()=> {setCount(count+1)}}>Add Me </button>
       <span>{count}</span>
       <button style={{margin:"10px"}} onClick={()=>setCount(count-1)}>Remove Me</button>
       <button onClick ={()=>setCount(0)}>Reset</button>
-          </div>
+      <ol>
+        {fruits.map((fruit, index)=>{
+          return <li key={index}> {fruit}</li>
+        }
+      )}
+      </ol>
+
+      </div>
   )
 }
