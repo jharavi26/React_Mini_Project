f94e43556f7ec869128486562ed67c8e78133877
Ravikumar Jha <jharavi153@gmail.com>
2024-12-23 16:25:03 +0530

    color-Picker

--- Project_Component/src/component/Four.jsx
+++ Project_Component/src/component/Four.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import { use } from 'react';
 
 const Four = ({items, itemsPerPage}) => {
+  const [color, setColor ] = useState('#000000')
   const [currentPage, setCurrentPage] = useState(1);
   const indexOfLastItem = currentPage*itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -10,6 +12,10 @@ const Four = ({items, itemsPerPage}) => {
   for(let i=0; i<=Math.ceil(items.length / itemsPerPage);i++){
     pageNumber.push(i);
   }
+
+  const handleChange = (e)=>{
+    setColor(e.target.value);
+  }
   return (
     <div>
       <ul>
@@ -17,6 +23,15 @@ const Four = ({items, itemsPerPage}) => {
           return <li key={index}>{item}</li>
         })}
       </ul>
+      {pageNumber.map((number) =>(
+        <li key={number} onClick={()=> setCurrentPage(number)}>
+          {number}
+        </li>
+      )
+
+      )}
+      <input type ="color"  onChange={handleChange}/>
+      <div style={{width : '100px', height:'100px', backgroundColor:color}} />
       
     </div>
   )
