2f3174b7847f24cb0366d1914e7e4b8a22de94b1
Ravikumar Jha <jharavi153@gmail.com>
2024-12-31 18:16:30 +0530

    Modal

---
+++ component/src/component/Modal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <button className='close-btn'></button>
+      <div className= "content"> Click the Button Below to accept amazing offer </div>
+      <button className="accept-btn" >Accept Offer</button>
+      
+    </div>
+  )
+}
+
+export default Modal
