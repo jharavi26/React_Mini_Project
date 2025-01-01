0e78d6e0886932eecc799f5e492b276e390e5f91
Ravikumar Jha <jharavi153@gmail.com>
2025-01-01 14:17:06 +0530

    Modal Completed

--- component/src/App.jsx
+++ component/src/App.jsx
@@ -3,13 +3,30 @@ import './App.css'
 import Modal from './component/Modal';
 
 function App() {
+  const[modalOpen, SetModalOpen] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleButtonClick = (value)=>{
+    SetModalOpen(false);
+    setMessage(value);
+  }
   
 
   return (
     <>
-    <Modal />
-   
-     
+    <button className='btn btn-open' onClick={()=> SetModalOpen(true)}>
+      Open </button>
+    {modalOpen && (
+      <Modal 
+      onSubmit={handleButtonClick} 
+      onCancel={handleButtonClick} 
+      onClose={handleButtonClick}>
+      <h1>This is the Modal Title</h1>
+      <p>This is the Description of the Modal</p>
+    </Modal>
+    )}
+
+{message && <p className="message">{message}</p>}
       
     </>
   )
--- component/src/component/Modal.jsx
+++ component/src/component/Modal.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import "./Modal.css"
 
-function Modal() {
+function Modal({onSubmit, onCancel, onClose, children}) {
   return (
     <>
-    <h1>Modal Component</h1>
+    <div className='modal-container' onClick={(e) =>{
+      if(e.target.className==="modal-container")
+        onClose("Modal was Closed ")
+    }}>
+    <div className='modal'>
+    <div className='modal-header' onClick ={()=> onClose("This modal was closed ")}> 
+      <p className='close'>&times;</p>
+    </div>
+    <div className='modal-content'>
+      {children}
+    </div>
+    <div className='modal-footer'>
+      <button className='btn btn-submit' onClick={()=> onSubmit("Submit Button was Clicked")}>Submit</button>
+      <button className='btn btn-cancel' onClick={()=>onCancel("Cancel The Modal")}>Cancel</button>
+    </div>
+    </div>
+    </div>
     </>
-   
   )
 }
 
