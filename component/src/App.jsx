2c211eca36ff7c0646401dac3ec4b9db82aea91a
Ravikumar Jha <jharavi153@gmail.com>
2025-01-01 12:42:29 +0530

    css implementation

--- component/src/App.jsx
+++ component/src/App.jsx
@@ -3,25 +3,12 @@ import './App.css'
 import Modal from './component/Modal';
 
 function App() {
-  const [isShow, setIsShow] = useState(false);
-
-  const handleModal = ()=>{
-    setIsShow(true);
-
-
-  }
-
+  
 
   return (
     <>
-    <div className='show-offer'>
-    <button onClick={handleModal} className='btn'>Show Offer </button>
-    </div>
-
-    {
-      isShow && < Modal />
-    }
-  
+    <Modal />
+   
      
       
     </>
--- component/src/component/Modal.jsx
+++ component/src/component/Modal.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
+import "./Modal.css"
 
 function Modal() {
   return (
-    <div className='modal'>
-      <button className='close-btn'></button>
-      <div className= "content"> Click the Button Below to accept amazing offer </div>
-      <button className="accept-btn" >Accept Offer</button>
-      
-    </div>
+    <>
+    <h1>Modal Component</h1>
+    </>
+   
   )
 }
 
