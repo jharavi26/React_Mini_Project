9928a8ce0cda4b96ae6981444f87ea4b42b0a8dc
Ravikumar Jha <jharavi153@gmail.com>
2025-01-13 18:39:19 +0530

    Update

--- Recat_Router/src/components/Navbar.jsx
+++ Recat_Router/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <img src="https://www.shutterstock.com/shutterstock/photos/2278726727/display_1500/stock-vector-minimalistic-circular-logo-sample-vector-2278726727.jpg" style={{ width: '100px', height: '80px' }} alt="Icon Image"/>
       <ul>
-        <Link to='/'><li>Home </li></Link>
-        <Link to='/products'><li>Products</li></Link>
-        <Link to='/about'><li>About</li></Link>
-        <Link to='/contact'><li>Contact</li></Link>
+        <NavLink to='/'><li>Home </li></NavLink>
+        <NavLink to='/products'><li>Products</li></NavLink>
+        <NavLink to='/about'><li>About</li></NavLink>
+        <NavLink to='/contact'><li>Contact</li></NavLink>
       </ul>
       <button>Get Started</button>
       
