3fb3e748e2f94fbe7e4f1c53ac59f23d5498a841
Ravikumar Jha <jharavi153@gmail.com>
2025-01-17 17:31:34 +0530

    Pagination

---
+++ project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import CountDown from './component/CountDown'
+
+function App() {
+  
+
+  return (
+    <>
+    <CountDown />
+    </>
+  )
+}
+
+export default App
---
+++ project/src/component/CountDown.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import './style.css'
+
+function CountDown() {
+
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchData = async ()=>{
+    const res = await fetch("https://dummyjson.com/products")
+    const data = await res.json()
+
+    console.log(products)
+
+    if(data && data.products){
+    setProducts(data.products)
+    }
+  };
+
+
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  return (
+    <div>
+      {
+        products.length>0 && <div className='products'>
+        {
+          products.slice(page*5-5, page*5).map((prc)=>{
+            return <span className="products__single" key={prc.id}>
+             <img src ={prc.thumbnail} alt={prc.title}/>
+            <span>
+              {prc.title}
+            </span>
+            </span>
+            
+          })
+        }
+        </div>
+      }
+      {
+        products.length>0 && <div className ="pagination">
+        {
+          [...Array(products.length / 5)].map((_ , i )=>{
+            return <span key = {i} >{i+1}</span> 
+          })
+        }
+          </div>
+      }
+      
+    </div>
+  )
+}
+
+export default CountDown
