8c743d85c3024c9d60730bd658f6583dfc11790c
Ravikumar Jha <jharavi153@gmail.com>
2025-01-24 11:13:44 +0530

    Implement Tic-Tok Game

--- project/src/component/Tic-Tac-Game/Game.jsx
+++ project/src/component/Tic-Tac-Game/Game.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
 import "./Game.css"
+import useTicToe from "./UseGame";
 
 function Game(){
 
-  const initialBoard = ()=>Array(9).fill(null);
-
-const [board, setBoard] = useState(initialBoard());
-
+  const {board, handleClick, calculatWinner, resetGame, getStatusMessage } = useTicToe();
 
   return (
     <div className="game">
-    <div className="status">Player X Turn
-      <button className="reset">Reset The Game</button>
+    <div className="status">{getStatusMessage()}
+      <button className="reset" onClick={resetGame}>Reset The Game</button>
 
     </div>
     <div className="board">
       {
-        board.map((_, index)=>{
-          return <button className="cell" key = {index}>X</button>
+        board.map((b, index)=>{
+          return <button className="cell" key = {index} onClick={()=>handleClick(index)}
+          disabled={b!==null}
+          >{b}</button>
         })
       }
       
--- project/src/component/Tic-Tac-Game/UseGame.jsx
+++ project/src/component/Tic-Tac-Game/UseGame.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import { useState } from "react";
 
-const UseGame = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+const initialBoard = ()=>Array(9).fill(null);
+
+
+const useTicToe = ()=>{
+  const [board, setBoard] = useState(initialBoard());
+  const [isnext, setIsNext] = useState(true);
+
+  const WINNING_PATTERNS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  const calculateWinner = (currebtBoard)=>{
+    for(let i=0; i<WINNING_PATTERNS.length; i++){
+      const [a,b,c] = WINNING_PATTERNS[i];
+      if(currebtBoard[a] && 
+        currebtBoard[a]===currebtBoard[b] && 
+        currebtBoard[a]===currebtBoard[c])
+        {
+          return currebtBoard[a];
+        }
+    }
+    return null;
+
+  }
+
+  const handleClick = (index)=>{
+    const winner = calculateWinner(board)
+    if(winner || board[index]) return 
+
+    const newBoard = [...board]
+    newBoard[index] = isnext ? "X" : "O";
+    setBoard(newBoard)
+    setIsNext(!isnext)
+
+  }
+
+  const getStatusMessage = ()=>{
+    const winner = calculateWinner(board);
+    if (winner) return `Player ${winner} wins`
+    if(!board.includes(null)) return `its a draw`;
+    return `Player ${isnext ? "X" : "O"} turn `
+
+  }
+
+  const resetGame = ()=>{
+    setBoard(initialBoard());
+    setIsNext(true);
+
+
+  }
+
+  return {board, handleClick, calculateWinner, getStatusMessage, resetGame}
+
 }
 
-export default UseGame
+export default useTicToe;
