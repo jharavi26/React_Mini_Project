a6a1692d60a93b22aac593f040508195b3855052
Ravikumar Jha <jharavi153@gmail.com>
2025-01-27 13:26:10 +0530

    Filter-data

--- project/src/component/Product_Filter/Product.jsx
+++ project/src/component/Product_Filter/Product.jsx
@@ -1,21 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Product.css"
 import { items } from './Items'
 
 const Product = () => {
 
-  const filter  = ['Bag', 'Watches', 'Sports', 'Sunglasses'];
+  const filter  = ['Bags', 'Watches', 'Sports', 'Sunglasses'];
   const[filterdata, setFilterData] = useState(items);
   const [activefilter, setActiveFilter] = useState([]);
 
+  const handleFilterClick = (e)=>{
+    const category = e.target.id;
+    if (activefilter.includes(category)){
+      const filters = activefilter.filter((el) => el !== category)
+      setActiveFilter(filters)
+    }
+    else{
+      setActiveFilter([...activefilter, category])
+    }  
+  }
+
+  const filterProduct = ()=>{
+    if(activefilter.length){
+      const tempItems = items.filter((item)=> activefilter.includes(item.category))
+        setFilterData(tempItems);
+      }
+    else{
+      setFilterData(items);
+    }
+  }
+
+  useEffect(()=>{
+    filterProduct();
+  },[activefilter])
+
   return (
     <div className='container'>
-      <div className='filter'>
+      <div className='filter' onClick={handleFilterClick}>
         <h1> Product Filter Component</h1>
         <div className='sort'>
         {
           filter.map((item,index)=>{
-            return <button className='item' key={index} id={item}>{item}</button>
+            return <button className={activefilter.includes(item) ? "selected" : ""} key={index} id={item}>{item}</button>
           })
         }
         </div>
