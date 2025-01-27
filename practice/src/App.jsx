2233c8275576077920c85100993d5afd93c980e0
Ravikumar Jha <jharavi153@gmail.com>
2025-01-27 18:29:29 +0530

    Handle_API

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,27 +1,62 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
-import Counter from './Component/Counter'
-import Focus from './Component/Focus';
 
 
 function App() {
 
-  let cars = ['tata', 'honda', 'maruti', 'hundai', 'toyota'];
- 
+  const [imageurl,  setImageUrl] = useState([]);
+  const [index, setIndex] = useState(0);
+  const ref  = useRef(null);
+
+  async function getData(){
+    const res = await fetch ("https://picsum.photos/v2/list");
+    const data = await res.json();
+    setImageUrl(data);
+  } 
+
+  useEffect(()=>{
+    getData();
+  },[])
+
+  useEffect(()=>{
+    if(imageurl.length > 0)
+    {
+    ref.current = setInterval(() => { 
+      handleRight();
+    }, 1000);
+  }
+    return ()=>clearInterval(ref.current)
+  },[imageurl]);
+
+  const handleLeft = ()=>{
+    setIndex((prev)=>(prev-1+imageurl.length) % imageurl.length)
+  }
+
+  const handleRight = ()=>{
+    setIndex((prev=>(prev+1) % imageurl.length))
+
+  }
   
   return (
     <>
-     <h1>React-Practice</h1>
-    <Focus />
-     <Counter name="Art" list={cars}/>
-     <ol>
-      {
-        cars.map((item)=>{
-          return <li key = {cars.index}>{item}</li>
-        })
+    <h1>Image Coursal</h1>
+    <div className='container' onMouseEnter={()=>clearInterval(ref.current)} 
+    onMouseLeave={()=>ref.current = setInterval(handleRight, 1000)}>
+    <button className='left-btn' onClick={handleLeft}>⬅️</button>
+      <div className='imagelist'>
+        { 
+        imageurl.length >0 ? (
+          <img src = {imageurl[index].download_url} key = {imageurl.id} className='image'></img>
+        ) : (
+          <p>Image is Loading</p>
+        )
+        }
+      </div>
+      <button className='right-btn' onClick={handleRight}>➡️</button>
 
-     }
-     </ol>
+    </div>
+
+    
      </>
   )
 }
