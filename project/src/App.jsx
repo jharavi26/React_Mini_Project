7cf045ca2db93b54246ce945064b646b31883804
Ravikumar Jha <jharavi153@gmail.com>
2025-01-28 16:24:58 +0530

    Stepper Implementation

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,20 +1,42 @@
 
 import { useEffect, useState } from 'react'
 import './App.css'
-import Product from './component/Product_Filter/Product'
+import Stepper from './component/Stepper/Stepper'
 
+const steps = [
+  {
+    label : "Personal-Info",
+    content : <div>Personal Information Content</div>,
+  },
 
+  {
+    label : "Account Info",
+    content : <div>Account Info Content</div>,
 
+  },
+  {
+    label : "Payment Info",
+    content : <div>Payment Content</div>,
 
+  },
+  {
+
+    label : "Confirmation",
+    content : <div>Confirmation Content</div>,
+  },
+  {
+    label : "Review-Info",
+    content : <div>Review Content</div>,
+  }
+]
 
 
 function App() {
 
   return (
     <div className='app'>
-      <Product />
-      
-    
+      <Stepper steps = {steps}/>
+     
       
     </div>
     
