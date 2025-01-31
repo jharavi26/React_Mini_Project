a57f8e0d5f808107a26603fb4acb4572003ce22d
Ravikumar Jha <jharavi153@gmail.com>
2025-01-31 12:33:31 +0530

    Selectable Grid

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -8,9 +8,12 @@ import Grid from './component/Select_Grid/Grid'
 function App() {
 
   return (
+ 
+   
     <div className ='app'>
-      <Grid/>
+      <Grid rows = {10} columns={10}/>
     </div>
+ 
     
   )
 }
