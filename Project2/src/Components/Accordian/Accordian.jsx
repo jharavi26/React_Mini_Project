dd6114f869f165612810505ffbabc1fe3336bde1
Ravikumar Jha <jharavi153@gmail.com>
2025-02-07 22:13:30 +0530

    Implement Accordian

---
+++ Project2/src/Components/Accordian/Accordian.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import "./style.css"
+
+function Accordian({qna}) {
+  const [show , setShow] = useState(false)
+  return (
+    <div className='accordian'>
+      <h3>{qna.question}<span onClick={()=>setShow(!show)}>{show  ? "-" :  "+"}</span></h3>
+      { show ? 
+      <p>{qna.answer}</p> : ""
+      } 
+      
+    </div>
+  )
+}
+
+export default Accordian
+
--- Project2/src/Components/Accordian/Faq.jsx
+++ Project2/src/Components/Accordian/Faq.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import data from "./data.json"
+import Accordian from './Accordian';
 
 
 function Faq() {
   return (
     <div>
       <h1>FAQ's</h1>
+      {
+        data.faq.map((obj , index)=>{
+          return <Accordian key = {index} qna ={obj} />
+        })
+      }
  
       
     </div>
