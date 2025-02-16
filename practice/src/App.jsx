601f79b65e965ce46f00b0831db97524c923c580
Ravikumar Jha <jharavi153@gmail.com>
2025-02-16 17:10:43 +0530

    Add Component

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,26 +1,35 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
+import Image from './Component/Image'
 
 
 function App() {
 
- 
+  const [user, setUser] = useState([])
 
+  useEffect(()=>{
+   
+  
+  },[])
 
+  async function GetData(){
+    const data = await fetch("https://dummyjson.com/users");
+    const result = await data.json();
+    setUser(result.users)
+  }
 
-  
-    
-  
-const handleChange =(e)=>{
- setValue(e.target.value)
-} 
-  
   return (
     <>
-    <h1>Debouncing Concept </h1>
-    <input type="text" placeholder='enter the text' value={value} onChange={handleChange}></input>
-    <p>{text}</p>
-    
+    <Image />
+    <button onClick={GetData}>GetData</button>
+    <p>{user.length > 0 ? user[3].firstName : "loading"}</p>
+    { 
+      user.length > 0 && (
+      user.map((item, index)=>{
+        return <li key={item.id}>{item.firstName}</li>
+      }))
+    }
+
      </>
   )
 }
---
+++ practice/src/Component/Image.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Image() {
+  return (
+    <div>
+     <h1>Fetch a Data from API</h1> 
+    </div>
+  )
+}
+
+export default Image
